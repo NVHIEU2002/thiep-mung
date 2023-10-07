faaa574a1a0af8f8150e14630b38aeb06b486dd0
NVHIEU2002 <100655122+NVHIEU2002@users.noreply.github.com>
2023-10-07 23:51:24 +0700

    Add files via upload
    Happy

---
+++ config.js
@@ -0,0 +1,13 @@
+const nameGirl = 'Chị';
+const giftUrl = 'http://nodemy.vn';
+const eventName = 'Happy birthday';
+const titleCard = 'HAPPY BIRTHADAY';
+const contentCard = 'Tuổi mới chúc chị sinh nhật vui vẻ , ngày càng xinh đẹp và sớm kiếm được người yêu nhaaaa :3 🤣';
+
+// phần dưới dành cho các bạn biết code, nếu muốn chỉnh ảnh đơn giản với base64
+// Cần hỗ trợ hãy liên hệ: 
+// Mr-Nam http://facebook.com/nam.nodemy
+// Các bạn muốn học lập trình thì tham gia Nhóm zalo tự học lập trình nhé: https://zalo.me/g/yhdkef092
+const giftImage = 'img-ngoc.jpg';
+const base64 = '';
+const giftImageBase64 = "data:image/png;base64, " + base64;
